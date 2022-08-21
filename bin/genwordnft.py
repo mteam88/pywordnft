045a9a80e5616c602ee59d77e1ccfd75f3c49b7f
mteam88 <matthew8edelen@gmail.com>
2022-08-21 15:04:19 -0400

    script working

--- bin/genwordnft.py
+++ bin/genwordnft.py
@@ -1,15 +1,16 @@
-#!/usr/bin/env python -i
+#!/usr/bin/env python
 
-import json
+from email.policy import default
+import click, json, os
 
-from pywordnft.genimage import * # replace *
+from pywordnft.genimage import Image
 
-images = list(Image.loadfromjson('imgdata.json'))
-print(images)
-
-import os
-if not os.path.exists('results'):
-    os.makedirs('results')
-
-for image in images:
-    image.draw().save(f'results/{image.title}.jpeg')
+@click.command()
+@click.argument('json-path', type=click.Path(exists=True))
+@click.argument('results-dir', type=click.Path(dir_okay=True, file_okay=False))
+@click.option('--output-fileext', default='jpeg')
+def drawimagesfromjson(json_path, results_dir, output_fileext='jpeg'):
+    if not os.path.exists(results_dir):
+        os.makedirs(results_dir)
+    for image in Image.loadfromjson(json_path):
+        image.draw().save(f'{os.path.abspath(results_dir)}/{image.title}.{output_fileext}')
